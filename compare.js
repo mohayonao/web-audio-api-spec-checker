f17a71473d7c5ba52fd016b75021f2284f687c97
mohayonao <mohayonao@gmail.com>
2017-01-23 16:28:50 +0900

    compare spec tools

---
+++ compare.js
@@ -0,0 +1,57 @@
+"use strict";
+
+function deepEqual(a, b) {
+  const akeys = Object.keys(a);
+  const bkeys = Object.keys(b);
+
+  if (akeys.length !== bkeys.length) {
+    return false;
+  }
+  if (akeys.some(key => !bkeys.includes(key))) {
+    return false;
+  }
+  return akeys.every((key) => {
+    return akeys[key] === bkeys[key];
+  });
+}
+
+function compare(specA, specB) {
+  specA = require(`./specs/${ specA }`);
+  specB = require(`./specs/${ specB }`);
+
+  const report = { "add": [], "remove": [], "moved": [], "change": [] };
+  const methods = [];
+
+  Object.keys(specB).sort().filter(apiPath => !specA[apiPath]).forEach((apiPath) => {
+    const [ className, methodName ] = apiPath.split("/").slice(1);
+
+    if (!report.add.includes(`/${ className }`)) {
+      report.add.push(apiPath);
+    } else {
+      methods.push(methodName);
+    }
+  });
+
+  Object.keys(specA).sort().filter(apiPath => !specB[apiPath]).forEach((apiPath) => {
+      const [ className, methodName ] = apiPath.split("/").slice(1);
+      if (methods.includes(methodName)) {
+        report.moved.push(apiPath);
+      } else if (!report.remove.includes(`/${ className }`)) {
+        report.remove.push(apiPath);
+      }
+  });
+
+  Object.keys(specA).sort().filter(apiPath => specB[apiPath]).forEach((apiPath) => {
+    if (!deepEqual(specA[apiPath], specB[apiPath])) {
+      report.change.push(apiPath);
+    }
+  });
+
+  global.console.log(report);
+  global.console.log(`add        : ${ report.add.length }`);
+  global.console.log(`remove     : ${ report.remove.length }`);
+  global.console.log(`moved      : ${ report.moved.length }`);
+  global.console.log(`change     : ${ report.change.length }`);
+}
+
+compare.apply(null, process.argv.slice(2));
